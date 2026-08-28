4b268a9bc5c1cffb38679ea59f20b38cb3b9c42f
palharsh470 <palharsh470@gmail.com>
2026-08-28 09:13:50 +0530

    refactor asgi.py to improve structure and readability

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,24 +1,25 @@
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-from core.middleware import JWTAuthMiddleware
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
+from core.middleware import JWTAuthMiddleware
 from core.routing import websocket_urlpatterns
-print("🔥 ASGI FILE LOADED")
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-
     "http": django_asgi_app,
-
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
             URLRouter(websocket_urlpatterns)
         )
     ),
-
 })
